8e2b16c899b46dca31749e1f571d8a5c43dad28c
Huy <ngiahuy@tma.com.vn>
2022-03-31 14:59:27 +0700

    tow way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-study';
+export class AppComponent{
+  // name='Tiep phan';
+  title = 'angular-study' ;
+  constructor() { }
+
+  detailForm = new FormGroup({
+    name: new FormControl(''),
+    age: new FormControl(0),
+  });
+
+  ngOnInit() {
+  }
+
+  getInfo() {
+    console.log('FORM', this.detailForm);
+    const value = this.detailForm.value;
+    console.log(value);
+
+  }
 }
